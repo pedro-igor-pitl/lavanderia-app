d7a7f973c992cc5b15f716e07fd7e4b945eedbb5
PedroIgor <pedro.igor.pitl@gmail.com>
2026-04-18 18:38:01 -0300

    Feat: Implementação para buscar,selecionar e definir valores por peças

--- app/cadastro-cliente.tsx
+++ app/cadastro-cliente.tsx
@@ -6,30 +6,42 @@ import {
   StyleSheet,
   Alert,
   ScrollView,
+  Modal,
 } from 'react-native';
-import { useState } from 'react';
 import { useApp } from '../components/AppContext';
 import { useRouter } from 'expo-router';
+import { useState, useEffect } from 'react';
+import api from '../app/services/api';
 
 export default function CadastroCliente() {
-  const { pecas = [], periodos = [] } = useApp();
-
   const router = useRouter();
+  const [modalVisible, setModalVisible] = useState(false);
+
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [telefone, setTelefone] = useState('');
   const [tipo, setTipo] = useState<'peso' | 'peca' | ''>('');
   const [pecasSelecionadas, setPecasSelecionadas] = useState<any[]>([]);
   const [valorKg, setValorKg] = useState('');
-  const [periodo, setPeriodo] = useState('');
+  const [pecas, setPecas] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function carregarPecas() {
+      try {
+        const response = await api.get('/pecas/listar');
+        setPecas(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar peças:', error);
+      }
+    }
+    carregarPecas();
+  }, []);
 
   const togglePeca = (item: any) => {
     const existe = pecasSelecionadas.find(p => p.id === item.id);
 
     if (existe) {
-      setPecasSelecionadas(prev =>
-        prev.filter(p => p.id !== item.id)
-      );
+      setPecasSelecionadas(prev => prev.filter(p => p.id !== item.id));
     } else {
       setPecasSelecionadas(prev => [
         ...prev,
@@ -46,12 +58,20 @@ export default function CadastroCliente() {
     );
   };
 
+  const formatarMoeda = (valor: string) => {
+    const numero = Number(valor) / 100;
+
+    return numero.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+  };
+
   const salvar = () => {
     if (
       !nome ||
       !telefone ||
       !tipo ||
-      !periodo ||
       (tipo === 'peso' && !valorKg) ||
       (tipo === 'peca' &&
         (pecasSelecionadas.length === 0 ||
@@ -65,138 +85,166 @@ export default function CadastroCliente() {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>Novo Cliente</Text>
+    <View style={{ flex: 1 }}>
 
-      <Text style={styles.label}>Nome *</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Digite o nome"
-        placeholderTextColor="#777"
-        value={nome}
-        onChangeText={setNome}
-      />
+      {/* MODAL */}
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalHeader}>
+          <Text style={styles.title}>Selecionar Peças</Text>
 
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Digite o email"
-        placeholderTextColor="#777"
-        value={email}
-        onChangeText={setEmail}
-      />
-
-      <Text style={styles.label}>Telefone *</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Digite o telefone"
-        placeholderTextColor="#777"
-        value={telefone}
-        onChangeText={setTelefone}
-      />
-
-      {/* Tipo */}
-      <Text style={styles.label}>Tipo de Cliente *</Text>
-
-      <View style={styles.row}>
-        <TouchableOpacity
-          style={[styles.radio, tipo === 'peso' && styles.radioActive]}
-          onPress={() => setTipo('peso')}
-        >
-          <Text style={styles.radioText}>Peso</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={[styles.radio, tipo === 'peca' && styles.radioActive]}
-          onPress={() => setTipo('peca')}
-        >
-          <Text style={styles.radioText}>Peça</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* KG */}
-      {tipo === 'peso' && (
-        <>
-          <Text style={styles.label}>Valor por Kg *</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Ex: 5.00"
-            placeholderTextColor="#777"
-            value={valorKg}
-            onChangeText={setValorKg}
-            keyboardType="numeric"
-          />
-        </>
-      )}
-
-      {/* Peças */}
-      {tipo === 'peca' && (
-        <>
-          <Text style={styles.label}>Selecione as peças *</Text>
-
-          {pecas.map((item) => {
-            const selecionada = pecasSelecionadas.find(p => p.id === item.id);
-
-            return (
-              <View
-                key={item.id}
-                style={[
-                  styles.card,
-                  selecionada && styles.cardSelected,
-                ]}
-              >
-                <TouchableOpacity onPress={() => togglePeca(item)}>
-                  <Text style={styles.text}>
-                    {selecionada ? '☑' : '☐'} {item.nome}
-                  </Text>
-                </TouchableOpacity>
-
-                {selecionada && (
-                  <TextInput
-                    style={styles.inputSmall}
-                    placeholder="R$"
-                    placeholderTextColor="#777"
-                    keyboardType="numeric"
-                    value={selecionada.precoCliente}
-                    onChangeText={(value) =>
-                      atualizarPreco(item.id, value)
-                    }
-                  />
-                )}
-              </View>
-            );
-          })}
-        </>
-      )}
-
-      {/* Período */}
-      <View style={styles.rowBetween}>
-        <Text style={styles.label}>Período *</Text>
-
-        <TouchableOpacity onPress={() => router.push('/cadastro-periodo')}>
-          <Text style={styles.addText}>+ Novo</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.row}>
-        {periodos.map((item) => (
           <TouchableOpacity
-            key={item.id}
-            style={[
-              styles.radio,
-              periodo === item.id && styles.radioActive,
-            ]}
-            onPress={() => setPeriodo(item.id)}
+            style={styles.closeContainer}
+            onPress={() => setModalVisible(false)}
           >
-            <Text style={styles.radioText}>{item.nome}</Text>
+            <Text style={styles.closeButton}>✕</Text>
           </TouchableOpacity>
-        ))}
-      </View>
+        </View>
+        <View style={[styles.modalContainer, { paddingTop: 0 }]}>
 
-      <TouchableOpacity style={styles.button} onPress={salvar}>
-        <Text style={styles.buttonText}>Salvar Cliente</Text>
-      </TouchableOpacity>
-    </ScrollView>
+          <ScrollView>
+            {pecas.map((item) => {
+              const selecionada = pecasSelecionadas.find(p => p.id === item.id);
+
+              return (
+                <TouchableOpacity
+                  key={item.id}
+                  style={[
+                    styles.itemLista,
+                    selecionada && styles.itemSelecionado
+                  ]}
+                  onPress={() => togglePeca(item)}
+                  activeOpacity={0.6}
+                >
+                  <Text style={styles.itemTexto}>{item.nome}</Text>
+
+                  <View style={styles.checkBox}>
+                    {selecionada && <Text style={styles.check}>✔</Text>}
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={styles.buttonText}>Confirmar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
+      {/* TELA PRINCIPAL */}
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>Novo Cliente</Text>
+
+        <Text style={styles.label}>Nome *</Text>
+        <TextInput style={styles.input} value={nome} onChangeText={setNome} />
+
+        <Text style={styles.label}>Email</Text>
+        <TextInput style={styles.input} value={email} onChangeText={setEmail} />
+
+        <Text style={styles.label}>Telefone *</Text>
+        <TextInput style={styles.input} value={telefone} onChangeText={setTelefone} />
+
+        {/* Tipo */}
+        <Text style={styles.label}>Tipo de Cliente *</Text>
+
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.radio, tipo === 'peso' && styles.radioActive]}
+            onPress={() => {
+              setTipo('peso');
+              setPecasSelecionadas([]);
+            }}
+          >
+            <Text style={styles.radioText}>Peso</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.radio, tipo === 'peca' && styles.radioActive]}
+            onPress={() => {
+              setTipo('peca');
+              setValorKg('');
+            }}
+          >
+            <Text style={styles.radioText}>Peça</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* PESO */}
+        {tipo === 'peso' && (
+          <>
+            <Text style={styles.label}>Valor por Kg *</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              value={formatarMoeda(valorKg || '0')}
+              onChangeText={(value) => {
+                const somenteNumeros = value.replace(/\D/g, '');
+                setValorKg(somenteNumeros);
+              }}
+            />
+          </>
+        )}
+
+        {/* PEÇAS */}
+        {tipo === 'peca' && (
+          <>
+            <Text style={styles.label}>Peças *</Text>
+
+            <TouchableOpacity
+              style={styles.selectButton}
+              onPress={() => setModalVisible(true)}
+            >
+              <Text style={styles.selectText}>
+                {pecasSelecionadas.length > 0
+                  ? `${pecasSelecionadas.length} selecionadas`
+                  : 'Selecionar peças'}
+              </Text>
+            </TouchableOpacity>
+
+            {pecasSelecionadas.map((item) => (
+              <View key={item.id} style={styles.card}>
+                <View style={{ flex: 1 }}>
+                  <Text style={styles.nomePeca}>{item.nome}</Text>
+                  <Text style={styles.subText}>Defina o valor do Kg</Text>
+                </View>
+
+                <View style={styles.precoContainer}>
+                  <Text style={styles.moeda}>R$</Text>
+
+                  <TextInput
+                    style={styles.inputPreco}
+                    keyboardType="numeric"
+                    value={formatarMoeda(item.precoCliente || '0').replace('R$', '')}
+                    onChangeText={(value) => {
+                      const somenteNumeros = value.replace(/\D/g, '');
+                      atualizarPreco(item.id, somenteNumeros);
+                    }}
+                  />
+                </View>
+
+                <TouchableOpacity
+                  style={styles.botaoRemover}
+                  onPress={() =>
+                    setPecasSelecionadas(prev =>
+                      prev.filter(p => p.id !== item.id)
+                    )
+                  }
+                >
+                  <Text style={styles.textRemover}>✕</Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </>
+        )}
+
+        <TouchableOpacity style={styles.button} onPress={salvar}>
+          <Text style={styles.buttonText}>Salvar Cliente</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -223,8 +271,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   inputSmall: {
-    backgroundColor: '#1C1C1E',
-    color: '#fff',
+    backgroundColor: '#303030',
+    color: '#119400',
     padding: 10,
     borderRadius: 8,
     width: 80,
@@ -264,12 +312,14 @@ const styles = StyleSheet.create({
   },
   card: {
     backgroundColor: '#1C1C1E',
-    padding: 12,
-    borderRadius: 10,
+    padding: 14,
+    borderRadius: 12,
     marginTop: 10,
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
+    gap: 10,
+    borderWidth: 1,
+    borderColor: '#2A2A2C',
   },
   cardSelected: {
     borderWidth: 1,
@@ -281,5 +331,138 @@ const styles = StyleSheet.create({
   addText: {
     color: '#2563EB',
     fontWeight: '600',
+  },
+  nomePeca: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: '600',
+  },
+
+  subText: {
+    color: '#777',
+    fontSize: 12,
+    marginTop: 2,
+  },
+
+  precoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#0F0F0F',
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+
+  moeda: {
+    color: '#00FF88',
+    fontWeight: '600',
+    marginRight: 4,
+  },
+
+  inputPreco: {
+    color: '#00FF88',
+    minWidth: 60,
+    fontWeight: '600',
+  },
+
+  botaoRemover: {
+    backgroundColor: '#2A2A2C',
+    width: 30,
+    height: 30,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  textRemover: {
+    color: '#FF4D4D',
+    fontWeight: 'bold',
+  },
+  selectButton: {
+    backgroundColor: '#1C1C1E',
+    padding: 14,
+    borderRadius: 10,
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#2A2A2C',
+  },
+
+  selectText: {
+    color: '#aaa',
+  },
+
+  modalContainer: {
+    flex: 1,
+    backgroundColor: '#0F0F0F',
+    padding: 20,
+  },
+
+  itemLista: {
+    backgroundColor: '#1A1A1D',
+    padding: 16,
+    borderRadius: 12,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#2A2A2C',
+  },
+
+  itemSelecionado: {
+    borderColor: '#2563EB',
+    backgroundColor: '#1E293B',
+    shadowColor: '#2563EB',
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    elevation: 3,
+  },
+
+  itemTexto: {
+    color: '#E5E7EB',
+    fontWeight: '600',
+    fontSize: 15,
+  },
+
+  checkBox: {
+    width: 26,
+    height: 26,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#3A3A3C',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  check: {
+    color: '#00FF88',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  closeContainer: {
+    backgroundColor: '#1F1F22',
+    width: 36,
+    height: 36,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#2A2A2C',
+  },
+
+  closeButton: {
+    color: '#FF4D4D',
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    paddingBottom: 10,
+    backgroundColor: '#0F0F0F',
+    borderBottomWidth: 1,
+    borderBottomColor: '#1F1F22',
   },
 });
