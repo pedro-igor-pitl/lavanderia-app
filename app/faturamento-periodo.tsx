968aaab48d15adb8c5d797871d337fc90f897a0e
PedroIgor <pedro.igor.pitl@gmail.com>
2026-05-20 18:28:23 -0300

    Feat: Implementação de consumo da API para relatorio e melhoria na tela de relatorio

--- app/faturamento-periodo.tsx
+++ app/faturamento-periodo.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import {
   View,
@@ -5,25 +6,58 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
+  ActivityIndicator,
 } from 'react-native';
 
-export default function FaturamentoPeriodo() {
-  const { clienteId, inicio, fim } = useLocalSearchParams();
+import api from '../app/services/api';
 
-  const dados = [
-    {
-      roll: 'ROLL-001',
-      data: '20/05/2026',
-      valorKg: 'R$ 12,00',
-      total: 'R$ 240,00',
-    },
-    {
-      roll: 'ROLL-002',
-      data: '21/05/2026',
-      valorKg: 'R$ 10,00',
-      total: 'R$ 180,00',
-    },
-  ];
+export default function FaturamentoPeriodo() {
+    const { clienteId, inicio, fim } = useLocalSearchParams();
+
+    const [dados, setDados] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        buscarRelatorio();
+    }, []);
+
+    const formatarData = (data: string) => {
+        const [dia, mes, ano] = data.split('/');
+
+        return `${ano}-${mes}-${dia}`;
+    };
+
+    const inicioFormatado = formatarData(inicio as string);
+    const fimFormatado = formatarData(fim as string);
+
+    const buscarRelatorio = async () => {
+    try {
+        setLoading(true);
+
+        const response = await api.get(
+        `/financeiro/relatorioRollsPorPeriodo?clienteId=${clienteId}&inicio=${inicioFormatado}&fim=${fimFormatado}`
+        );
+
+        console.log(response.data);
+
+        setDados(response.data);
+
+    } catch (error) {
+        console.log('Erro ao buscar relatório:', error);
+    } finally {
+        setLoading(false);
+    }
+    };
+
+  const totalKg = dados.reduce(
+    (acc, item) => acc + Number(item.peso || 0),
+    0
+  );
+
+  const totalReais = dados.reduce(
+    (acc, item) => acc + Number(item.total || 0),
+    0
+  );
 
   return (
     <ScrollView style={styles.container}>
@@ -36,7 +70,9 @@ export default function FaturamentoPeriodo() {
           </View>
 
           <View style={styles.clienteInfo}>
-            <Text style={styles.nomeCliente}>Nome Cliente</Text>
+            <Text style={styles.nomeCliente}>
+              {dados[0]?.clienteNome}
+            </Text>
 
             <Text style={styles.periodo}>
               {inicio} - {fim}
@@ -49,46 +85,79 @@ export default function FaturamentoPeriodo() {
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.whatsappButton}>
-              <Text style={styles.whatsappText}>Enviar para WhatsApp</Text>
+              <Text style={styles.whatsappText}>
+                Enviar para WhatsApp
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
 
-        {/* TABELA */}
-        <View style={styles.tableContainer}>
-          {/* Header */}
-          <View style={[styles.row, styles.headerRow]}>
-            <Text style={[styles.cell, styles.headerCell]}>Roll</Text>
-            <Text style={[styles.cell, styles.headerCell]}>Data</Text>
-            <Text style={[styles.cell, styles.headerCell]}>Valor KG</Text>
-            <Text style={[styles.cell, styles.headerCell]}>
-              Total Referente
-            </Text>
-          </View>
+        {/* LOADING */}
+        {loading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <>
+            {/* TABELA */}
+            <View style={styles.tableContainer}>
+              {/* Header */}
+              <View style={[styles.row, styles.headerRow]}>
+                <Text style={[styles.cell, styles.headerCell]}>
+                  Roll
+                </Text>
 
-          {/* Dados */}
-          {dados.map((item, index) => (
-            <View style={styles.row} key={index}>
-              <Text style={styles.cell}>{item.roll}</Text>
-              <Text style={styles.cell}>{item.data}</Text>
-              <Text style={styles.cell}>{item.valorKg}</Text>
-              <Text style={styles.cell}>{item.total}</Text>
+                <Text style={[styles.cell, styles.headerCell]}>
+                  Data
+                </Text>
+
+                <Text style={[styles.cell, styles.headerCell]}>
+                  Valor KG
+                </Text>
+
+                <Text style={[styles.cell, styles.headerCell]}>
+                  Total Referente
+                </Text>
+              </View>
+
+              {/* Dados */}
+              {dados.map((item, index) => (
+                <View style={styles.row} key={index}>
+                  <Text style={styles.cell}>
+                    {item.roll || item.codigoManual}
+                  </Text>
+
+                  <Text style={styles.cell}>
+                    {item.data || item.dataColeta}
+                  </Text>
+
+                  <Text style={styles.cell}>
+                    R$ {item.peso}
+                  </Text>
+
+                  <Text style={styles.cell}>
+                    R$ {item.total}
+                  </Text>
+                </View>
+              ))}
             </View>
-          ))}
-        </View>
 
-        {/* TOTAIS */}
-        <View style={styles.footer}>
-          <View style={styles.totalBox}>
-            <Text>Total KG</Text>
-            <Text>120 KG</Text>
-          </View>
+            {/* TOTAIS */}
+            <View style={styles.footer}>
+              <View style={styles.totalBox}>
+                <Text>Total KG</Text>
 
-          <View style={styles.totalBox}>
-            <Text>Total em Reais</Text>
-            <Text>R$ 420,00</Text>
-          </View>
-        </View>
+                <Text>{totalKg.toFixed(2)} KG</Text>
+              </View>
+
+              <View style={styles.totalBox}>
+                <Text>Total em Reais</Text>
+
+                <Text>
+                  R$ {totalReais.toFixed(2)}
+                </Text>
+              </View>
+            </View>
+          </>
+        )}
       </View>
     </ScrollView>
   );
--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // Android emulator
+  baseURL: "http://192.168.0.246:8080", // Android emulator
 });
 
 export default api;
